262d007a225a6a10d2020fbd3efedf12eeb1e782
KevinOluoch <kevin.o.oluoch@gmail.com>
2016-12-20 09:24:56 +0300

    Make changes to make the program run
    The function factorial has a syntax change, from use of `expression`
    to $(expression), for mathematical expressions. Its Variables j and k,
    plus steps involving them, have been removed, and replaced with a
    line that executes and return results, so as to avoid unneccessary
    steps in the program.
    The while loop, now has a break command. Future changes that
    check on data intergrity, have been partially implemented and have
    thus been commented out.

--- factorial.sh
+++ factorial.sh
@@ -2,20 +2,26 @@
  
 factorial() 
 { 
-  if [ "$1" ­-gt 1 ]; then 
-    i=`expr $1 ­ 1` 
-    j=`factorial $i` 
-    k=`expr $1 \* $j` 
-    echo $k 
+  if [ "$1" -gt "1" ]; then 
+    i=$(($1 - 1))
+   #echo before 1 - $1 i- $i 
+    factorial $i
+    return $(($1 * $?))  
   else 
-    echo 1 
+    return 1 
   fi 
 } 
  
  
 while : 
 do 
-  echo "Enter a number:" 
+  echo "Enter a number (or anything else to exit):" 
   read x 
-  factorial $x 
+  #if [ $x =~ ^[0-9]+$ ]; then
+      factorial $x
+      echo The factorial of $x is $?
+  #else
+      echo bye
+      break
+#fi 
 done 
